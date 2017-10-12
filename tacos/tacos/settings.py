badf564c937b437492555a6367ec8868bca3785a
Akshay Desai <akshaydesai@dhcp-10-4-10-124.wireless.rochester.edu>
2017-10-12 18:22:24 -0400

    minor change

--- tacos/tacos/settings.py
+++ tacos/tacos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g+vuxub43tb_)@08m_i#9iv@v6$cwl8uxdh%r9tx=-*!cyy+(r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIS_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
