c025bd5ff294b2fbeaf0673e8b1ea2caa38df2c2
akshaydesai <adesai16>
2017-10-21 19:57:04 -0400

    form & view

--- tacos/findafriend/views.py
+++ tacos/findafriend/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
+from .forms import NewGroupForm
+from .models import Group
+
 
 def signup(request):
     if request.method == 'POST':
@@ -23,4 +26,17 @@ def signup(request):
 
 def home(request):
     users = {'user':request.user}
-    return HttpResponse(get_template('home.html').render(users))
+    return HttpResponse(get_template('findafriend/home.html').render(users))
+
+def newGroup(request):
+    if request.method == "POST":
+        form = NewGroupForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.creator = {'user':request.user}
+            post.timeCreated = timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = NewGroupForm()
+    return render(request, 'findafriend/create_group.html', {'form': form})
