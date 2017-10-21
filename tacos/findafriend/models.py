ef0931bb03c2927d0c1589af8a69305a81bbb06d
akshaydesai <adesai16>
2017-10-21 19:20:50 -0400

    Group Model

--- tacos/findafriend/models.py
+++ tacos/findafriend/models.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+SIZEOPTIONS =(
+	('Under 5', 'Under 5'),
+	('Under 5', 'Under 20'),
+	('Under 20', 'Under 50'),
+	('More than 50', 'More than 50'),
+	)
+
+class Group(models.Model):
+	title = models.CharField(max_length=200)
+	creator = models.ForeignKey('auth.User')
+	sizeOfGroup = models.CharField(max_length=12, choices=SIZEOPTIONS)
+	description = models.TextField()
+	timeCreated = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.title
