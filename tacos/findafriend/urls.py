7d98c061c280e88d75cdd3ee4a1d62cd56119d5f
slitzky28 <slitzky@u.rochester.edu>
2017-11-17 21:53:13 -0500

    commented out url

--- tacos/findafriend/urls.py
+++ tacos/findafriend/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^newGroup/$', views.newGroup, name='newGroup'),
 	url(r'^editProfile/$', views.editProfile, name='editProfile'),
+	#url(r'^search/$', views.groupSearch, name='search')
+	
 ]
