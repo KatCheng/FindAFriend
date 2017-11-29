3ac72c9557dca18ed53f0a50de4c59a0a47c08e3
Yeonwoo Lee <ylee100@u.rochester.edu>
2017-11-29 15:52:58 -0500

    websockets

--- angular-findafriend/src/app/group-message/group-message.component.ts
+++ angular-findafriend/src/app/group-message/group-message.component.ts
@@ -1,50 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { WebsocketService } from './websocket.service';
+//import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-group-message',
   templateUrl: './group-message.component.html',
-  styleUrls: ['./group-message.component.css']
+  styleUrls: ['./group-message.component.css'],
+  providers: [ WebsocketService, ChatService ]
 })
 
 export class GroupMessageComponent implements OnInit {
 	@Input() group: any;
-  @Input() username:string;
-  
-  messages:any = [
-  { sender: 'akshay', messageContent: 'Mr. Nice' },
-  { sender: 'akshay', messageContent: 'hello' },
-  { sender: 'akshay', messageContent: 'my' },
-  { sender: 'akshay', messageContent: 'name' },
-  { sender: 'akshay', messageContent: 'is ' },
-  { sender: 'akshay', messageContent: 'akhsya' },
-  
-];
+  	@Input() username:string;
+ 	echoMessage = '';
 
+  	messages:any = [
+  		{ sender: 'akshay', messageContent: 'Mr. Nice' },
+  		{ sender: 'akshay', messageContent: 'hello' },
+  		{ sender: 'akshay', messageContent: 'akhsya' },
+  	];
+	constructor(private chatService: ChatService) {
+		chatService.messages.subscribe(msg=>{
+			console.log("Websocket giving response: "+ msg);
+		});
+	}
+	
+	constructor(private http: HttpClient) { };
 
-  constructor(private http: HttpClient) { }
+	private message = {
+		sender: 'Yeonwoo',
+		messageContent: "this is testing messages"
+	}
 
-  ngOnInit() {
-  }
+  	sendMsg() {
+		console.log('new message from client to websocket: ', this.message);
+		this.chatService.messages.next(this.message);
+		this.message.message = '';
+	}
 
-  getMessages():void{
-  	// let url:string;
-  	// url = "/api/messages/?format=json";
-
-   //  console.log(url);
-
-  	// this.http.get(url).subscribe(data => {
-   //    // let obj = Group: JSON.parse(data);
-   //    // console.log(obj.title);
-   //    this.groups = data;
-   //    console.log(this.groups);
-   //  })
-  }
-
-  sentMessage(msg:string){
-    console.log("here");
-    console.log(msg);
-  }
-
+	ngOnInit() { };
 
 }
