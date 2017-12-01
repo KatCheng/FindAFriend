9c6cbf3f057635c801cbbb5166aa93fb5dc88e94
aollarve <aollarve@u.rochester.edu>
2017-12-01 11:50:52 -0500

    Added authentication in django

--- tacos/tacos/newapi/urls.py
+++ tacos/tacos/newapi/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 from django.contrib.auth import views
 from findafriend.views import home, signup, deleteUser, chatDirect
 from rest_framework import routers
-from .views import PageDetail, PageSearch, PageRetrieve, ProfileViewSet, ProfileAPIView, UserCreateAPIView, UserLoginAPIView
+from .views import (PageDetail, PageSearch, PageRetrieve, ProfileViewSet, ProfileAPIView, 
+UserCreateAPIView, UserLoginAPIView)
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
@@ -15,6 +16,7 @@ urlpatterns = [
 	url(r'^api/profiles/(?P<user>[\w|\W]+)/$', ProfileAPIView.as_view(), name='profiles'),
 	url(r'^api/login/$', UserLoginAPIView.as_view(), name='login'),
  	url(r'^api/register/$', UserCreateAPIView.as_view(), name='register'),
+ 	# url(r'^api/logout/$', UserLogoutAPIView.as_view(), name='logout'),
 ]
 
 
