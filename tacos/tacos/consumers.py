20798d9833a86ea8791e873ab930d046cbee74d8
Yeonwoo Lee <ylee100@u.rochester.edu>
2017-12-06 14:51:10 -0500

    Only using Websocket to connect the chatting

--- tacos/tacos/consumers.py
+++ tacos/tacos/consumers.py
@@ -4,9 +4,7 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 from findafriend.models import Page, Chat
 from django.contrib.auth.models import User
 import json
-import logging
 
-log = logging.getLogger(__name__)
 
 @channel_session_user_from_http
 def ws_con(msg):
@@ -15,27 +13,12 @@ def ws_con(msg):
         "accept": True
     })
 
-    # display
-#    if Chat.objects.filter(recipient=u) is not None:
- #       qs = Chat.objects.filter(recipient=data['recipient'])
-  #      t = "<p>"
-   #     for e in qs.iterator():
-    #        t += (e.messageContent + "</p>")
-     #   msg.reply_channel.send({
-      #      "text":t
-       # })
-    
     # add to the chat group
-    Group(msg.user.username).add(msg.reply_channel); 
+    Group("chat").add(msg.reply_channel); 
 
 # Connected to websocket.receive
 @channel_session_user
 def ws_msg(msg):
-    # echo to sender
-    #msg.reply_channel.send({
-    #"text": msg.content['text'],
-    #})
-
     data = json.loads(msg.content['text'])
 
     # history calling from request
@@ -56,14 +39,11 @@ def ws_msg(msg):
         c = Chat(sender=User.objects.get(username=data['sender']), recipient=Page.objects.get(title = data['recipient']), messageContent=data['message'])
         c.save()
     
-        log.debug("recipient=%s message=%s", data['recipient'], data['message'])
         # send message to the group
-        #for m in Page.objects.get(title=data['recipient']).members.all(): 
-        Group(msg.user.username).send({
+        Group("chat").send({
         "text": json.dumps(data)     
         })
 
-    
 @channel_session_user
 def ws_discon(msg):
-    Group(msg.user.username).discard(msg.reply_channel)
+    Group("chat").discard(msg.reply_channel)
