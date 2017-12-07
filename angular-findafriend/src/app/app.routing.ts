2e3a5e747dd4b69d1c43ba5b7c99f190b13a04a1
aollarve <aollarve@u.rochester.edu>
2017-12-07 14:41:08 -0500

    further error check

--- angular-findafriend/src/app/app.routing.ts
+++ angular-findafriend/src/app/app.routing.ts
@@ -6,9 +6,6 @@ import { AuthGuard } from './auth.guard';
 
 
 import { HomeComponent }   from './home/home.component';
-import { GroupsComponent } from './groups/groups.component';
-import {UsersComponent} from './users/users.component';
-//import { GroupDetailComponent } from './group-detail/group-detail.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -16,13 +13,8 @@ import { SignupComponent } from './signup/signup.component';
 
 const appRoutes: Routes = [
 	{ path: '', component: HomeComponent, canActivate: [AuthGuard]},
-	//{ path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
 	{ path: 'login', component: LoginComponent},
- //  { path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
-	// { path: 'groups', component: GroupsComponent, canActivate: [AuthGuard]},
   { path: 'signup', component: SignupComponent},
-    //{ path: 'login', component: LoginComponent },
-    //{ path: '', component: HomeComponent, canActivate: [AuthGuard] },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
