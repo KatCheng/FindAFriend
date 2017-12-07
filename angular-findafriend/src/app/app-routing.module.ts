7673b1057034895d90537197d82f3bfa4883722c
aollarve <aollarve@u.rochester.edu>
2017-12-07 14:37:56 -0500

    Fixed error in routing

--- angular-findafriend/src/app/app-routing.module.ts
+++ angular-findafriend/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GroupsComponent }      from './groups/groups.component';
-import {UsersComponent} from './users/users.component';
-//import { GroupDetailComponent }  from './group-detail/group-detail.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full'},
-  { path: 'groups', component: GroupsComponent },
-  { path: 'users', component: UsersComponent },
-  //{ path: 'detail/:group.title', component: GroupDetailComponent }
+  { path: '/groups', component: GroupsComponent },
 ];
 
 @NgModule({
