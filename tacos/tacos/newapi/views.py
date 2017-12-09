abb54d323f13637b1f9b9e0e92ab73e4d61f0b48
aollarve <aollarve@u.rochester.edu>
2017-12-09 12:30:57 -0500

    deleted further comments

--- tacos/tacos/newapi/views.py
+++ tacos/tacos/newapi/views.py
@@ -27,8 +27,6 @@ from findafriend.models import Page, UserProfile, Chat
 
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt 
-#from rest_framework.renderers import JSONRenderer
-#from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from tacos.newapi.permissions import IsOwnerOrReadOnly
 
