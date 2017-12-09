9e91c4674ed1bf0665fee2e63b9380a3ca88565b
Yeonwoo Lee <ylee100@u.rochester.edu>
2017-12-09 11:38:58 -0500

    Why`

--- tacos/findafriend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	url(r'^newGroup/$', views.newGroup, name='newGroup'),
-	url(r'^editProfile/$', views.editProfile, name='editProfile'),
-	#url(r'^search/$', views.groupSearch, name='search')
-	
-]
--- tacos/tacos/urls.py
+++ tacos/tacos/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
 from django.contrib.auth import views
-from findafriend.views import home, signup, deleteUser, chatDirect
 from rest_framework import routers
 from tacos.newapi import views
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -13,7 +12,6 @@ router.register(r'api/users', views.UserViewSet)
 router.register(r'api/pages', views.PageViewSet)
 router.register(r'api/profiles', views.ProfileViewSet)
 router.register(r'api/messages', views.ChatViewSet)
-router.register(r'api/chatrooms', views.ChatRoomViewSet)
 router.register(r'api/joinGroup/(?P<group>.+)/(?P<username>.+)', views.JoinGroupSet)
 router.register(r'api/leaveGroup/(?P<group>.+)/(?P<username>.+)', views.LeaveGroupSet)
 router.register(r'api/deleteGroup/(?P<group>.+)', views.DeleteGroupSet)
@@ -27,13 +25,13 @@ router.register(r'api/deleteUser/(?P<user>.+)', views.DeleteUser)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/signup/$', signup, name='signup'),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout'),
-    url(r'^$', home, name='home'),
-    url(r'',include('findafriend.urls')),
-    url(r'^accounts/delete/$', deleteUser, name='delete'),
-    url(r'^chat/(?P<recipient>[\w-]+)/$', chatDirect, name='chat'),
+#    url(r'^accounts/signup/$', signup, name='signup'),
+#    url(r'^accounts/login/$', views.login, name='login'),
+#    url(r'^accounts/logout/$', views.logout, name='logout'),
+#    url(r'^$', home, name='home'),
+#    url(r'',include('findafriend.urls')),
+#    url(r'^accounts/delete/$', deleteUser, name='delete'),
+#    url(r'^chat/(?P<recipient>[\w-]+)/$', chatDirect, name='chat'),
     url(r'', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'', include('tacos.newapi.urls')),
